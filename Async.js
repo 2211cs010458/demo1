e6a512f317197da961c9e867470768fcd261c612
web-dev <sagararavind87@gmail.com>
2025-01-22 19:15:06 +0530

    Async

--- Async.js
+++ Async.js
@@ -12,3 +12,52 @@
 
 
 //! Promise
+// let p1 = new Promise(() => { });
+// console.log(p1);
+// let p2 = new Promise((resolve, reject) => { 
+//     resolve("Success")
+// });
+// p2.then((res) => {
+//     console.log(res);
+// });  
+// p2.finally(() => console.log(final));
+
+// // console.log(p2);
+// let p3 = new Promise((resolve, reject) => {
+//     reject("Failure")
+// });
+//     p3
+//     .then(res => console.log(res))
+//     .catch(err => console.log(err))
+//     .finally(() => console.log("final"))
+
+// console.log(p3);
+
+
+// API fetching
+
+function fetchUsers(){
+    let response = fetch("https://jsonplaceholder.typicode.com/users");
+    // console.log(response);
+    response.then(res=>{
+        // console.log(res)
+        // console.log(res.json());
+        return res.json().then(data=>{
+            // console.log(data);
+            let store = document.getElementById("store");
+            data.map(user=>{
+                console.log(user);
+                store.innerHTML +=`
+                <tr>
+                <td>${user.id}</td>
+                <td>${user.name}</td>
+                <td>${user.email}</td>
+                <td>${user.company.name}</td>
+                </tr>
+                `
+            })
+        })
+    })
+    .catch(err=>console.log(err))
+}
+fetchUsers();
