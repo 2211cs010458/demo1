8c162a6fd18bf7ad675e23972fe89575e61bb6af
web-dev <sagararavind87@gmail.com>
2025-01-25 18:00:48 +0530

    dom

--- dom.js
+++ dom.js
@@ -8,7 +8,7 @@
 // test.innerHTML = "<h1>Header</h1>"
 // console.log(test);
 
-// ! 
+// !
 
 // let ele = document.getElementsByClassName("test");
 // ele[0].style.backgroundColor="dodgerblue"
@@ -36,14 +36,14 @@
 //     console.log(element.innerText);
 // })
 
-//! 
+//!
 // let ele = document.getElementsByName("username");
 // // console.log(ele , Array.isArray(ele));
 // [...ele].map(u=>{
 //     console.log(u.value);
 // })
 
-//! 
+//!
 // let ele = document.querySelector("#demo");
 // console.log(ele);
 // let ele = document.querySelector(".test");
@@ -66,12 +66,71 @@
 //     console.log("double click");
 // })
 
-let demo =document.getElementById("demo");
+// let demo =document.getElementById("demo");
 
-demo.addEventListener("mouseover",()=>{
-    document.body.style.backgroundColor="red";
-})
+// demo.addEventListener("mouseover",()=>{
+//     document.body.style.backgroundColor="red";
+// })
 
-demo.addEventListener("mouseleave",()=>{
-    document.body.style.backgroundColor="teal";
-})
+// demo.addEventListener("mouseleave",()=>{
+//     document.body.style.backgroundColor="teal";
+// })
+// let red = document.getElementById("red");
+// red.addEventListener("mouseover", () => {
+//     document.body.style.backgroundColor = "red";
+// });
+// let green = document.getElementById("green");
+// green.addEventListener("mouseover", () => {
+//     document.body.style.backgroundColor = "green";
+// });
+// let blue = document.getElementById("blue");
+// blue.addEventListener("mouseover", () => {
+//     document.body.style.backgroundColor = "blue";
+// });
+// let teal = document.getElementById("teal");
+// teal.addEventListener("mouseover", () => {
+//     document.body.style.backgroundColor = "teal";
+// });
+// let yellow = document.getElementById("yellow");
+// yellow.addEventListener("mouseover", () => {
+//     document.body.style.backgroundColor = "yellow";
+// });
+
+    
+//////////////////////////////////////////
+ 
+// const colors = ["red", "green", "blue", "teal", "yellow"];
+
+// colors.forEach(color => {
+//     let element = document.getElementById(color);
+//     element.addEventListener("mouseover", () => {
+//         document.body.style.backgroundColor = color;
+//     });
+// });
+
+// let bgcolor = document.querySelectorAll(".bgcolor");
+
+// [...bgcolor].map(element => {
+//     element.addEventListener("mouseover", () => {
+//         element.style.backgroundColor = element.innerText; // Corrected property: innerText
+//     });
+
+//     element.addEventListener("mouseleave", () => {
+//         element.style.backgroundColor = "white";
+//     });
+// });
+
+
+let ele = document.createElement("h1");
+ele.innerText = "dynamic content html discrete";
+ele.setAttribute("id", "demo");
+console.log(ele);
+
+document.body.appendChild(ele);
+
+let img = document.createElement("img");
+
+img.src = "./3606208.jpg"; 
+console.log(img);
+
+document.body.appendChild(img);
