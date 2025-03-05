66d2ff5b4ad6347cef8bf84a640d223cdac083ca
web-dev <sagararavind87@gmail.com>
2025-03-05 18:49:42 +0530

    frontend

--- frontend/MRUFrontend/src/Laptops.jsx
+++ frontend/MRUFrontend/src/Laptops.jsx
@@ -1,19 +1,36 @@
-import axios from "axios";
 import { useEffect, useState } from "react";
-const Laptops = ()=>{
-    const [laptops,setLaptops] = useState([]);
-    const get_laptops = async ()=>{
-        const res = await axios.get("http://localhost:9001/laptops");
-        const {data} = res;
-        setLaptops(data);
+import axios from "axios";
+import "./Laptops.css";
+
+const Laptops = () => {
+    const [laptops, setLaptops] = useState([]);
+
+    const getLaptops = async () => {
+        try {
+            const res = await axios.get("http://localhost:9001/laptops");
+            setLaptops(res.data);
+        } catch (error) {
+            console.error("Error fetching laptops:", error);
+        }
     };
-    useEffect(()=>{
-        get_laptops();
-    },[]);
-    return(
-        <>
-            <p>{JSON.stringify(laptops)}</p>
-        </>
-    )
-}
+
+    useEffect(() => {
+        getLaptops();
+    }, []);
+
+    return (
+        <div className="parent">
+            {laptops.map((element) => (
+                <div key={element.id} className="child">
+                    <img src={element.pimage} alt={element.pname} />
+                    <h2>
+                        <i className="fa fa-rupee"></i> {element.pcost}
+                    </h2>
+                    <p>Qty: {element.qty}</p>
+                </div>
+            ))}
+        </div>
+    );
+};
+
 export default Laptops;
